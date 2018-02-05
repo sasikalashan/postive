d76f04c7b2e308d33a7c3d8f210531c0e7c2ccd9
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-02-05 21:44:56 +0530

    Update arthmatic progression.c

--- arthmatic progression.c
+++ arthmatic progression.c
@@ -1,25 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int i,n,a,d,tn,sum=0;
-printf("enter the number:");
+int a,d,n,value,i,sum=0;
+printf("enter the terms:");
 scanf("%d",&n);
 printf("enter the intial term:");
-scanf("%d",&a
+scanf("%d",&a);
 printf("Enter the difference element:");
 scanf("%d",&d);
-sum=(n(2a+(n-1)d))/2;
-tn=a+(n-1)d;
-for(i=0;i<=tn;i=i+d)
-{
-if(i!=tn)
-{
-printf("%d+",i);
-}
-else
-{
-print("%d=%d",i,sum);
-}
+value=a;
+printf("AP series");
+for(i=0;i<n;i++)
+  {
+printf("%d", value);
+sum=sum+ value;
+value=value+d;
+  }
+printf("sum of AP series till %d terms is=%d",n,sum);
 return 0;
 }
 
