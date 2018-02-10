f1134b3bb8d1a60bc685a968d4271f519d30db79
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-02-10 13:26:58 +0530

    Update array max element.c

--- array max element.c
+++ array max element.c
@@ -5,16 +5,17 @@ int a[100],i,max,size;
 printf("Enter the array size:");
 scanf("%d",&size);
   printf("The %d element:",size);
-for(i=0;i<=size;i++)
+for(i=0;i<size;i++)
 {
 scanf("%d",&a[i]);
 }
 max=a[0];
-for(i=1;i<=size;i++)
+for(i=1;i<size;i++)
 {
 if(max<a[i])
 {
 max=a[i];
+  a[i]=max;
 }
 }
 printf("the max element is:%d",max);
