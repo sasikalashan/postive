c2fb371765bc0d8d8ed736d732ad84688c0b1626
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-02-20 21:58:16 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -4,6 +4,8 @@ int main()
 int a,b,c;
 printf("Enter the numbers:");
 scanf("%d %d",&a,&b);
-printf("Befor swapping the two numbera a=%d b=%d",a,b);
+printf("\nBefor swapping the two numbera a=%d b=%d",a,b);
 c=a;a=b;b=c;
-printf("
+printf("\nafter swapping a=%d and b=%d",a,b);
+  return 0;
+  }
