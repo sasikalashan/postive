33e041b8db7d347377a9ff343ac74a5819458d41
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-02-21 06:21:25 +0530

    Update mediant  of array.c

--- mediant  of array.c
+++ mediant  of array.c
@@ -1,27 +1,15 @@
 #include<stdio.h>
 int main()
 {
-int a[50],i,j,n,m;
+int a[50],i,j=0,n,m;
 printf("Enter the size:");
 scanf("%d",&n);
-printf("The %d element:\n");
-for(i=0;i<n;i++)
+printf("The %d element:\n",n);
+for(i=0;i<n;i++) 
 {
 scanf("%d",&a[i]);
-}
-for(i=0;i<n;i++)
-{
-for(j=i+1;j<n;j++)
-{
-if(a[i]<a[j])
-{
-m=a[i];
-a[i]=a[j];
-a[j]=m;
-}
-}
-}
-printf("%d\t",a[i]);
-printf("the 2 nd large number:%d",a[n-2]);
+  j=j+a[i];
+  }m=j/n;
+  printf("The medium element:%d",m);
 return 0;
 }
