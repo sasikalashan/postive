046cf99713f7ee86f773d7799cc533586fa6f31a
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-02-26 15:52:37 +0530

    Create no of word in sentence.c

---
+++ no of word in sentence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a[20];
+int c=0,i;
+printf("Enter the sentence:");
+gets(a);
+for(i=0;a[i]!=NULL;i++)
+{
+if(a[i]==' ')
+{
+c++;
+}
+}
+printf("The number of word in sentence:%d",c+1);
+return 0;
+}
