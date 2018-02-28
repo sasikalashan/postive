0e4f1cdd09569fd522f3f156dd5ac20b7e6a6c2f
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-02-28 16:45:16 +0530

    Create fibonacci series.c

---
+++ fibonacci series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,num,f1=0,f2=1,c;
+printf("Enter the range of series:");
+scanf("%d",&num);
+printf("Fibonacci series:");
+for(i=0;i<num;i++)
+{
+printf("\n%d",f1);
+c=f1+f2;
+f1=f2;
+f2=c;
+}
+return 0;
+}
