b479de9ef2e9f2df4ef9074c8ea9b7f5ffaad515
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-03-14 16:03:56 +0530

    Update average of n num.c

--- average of n num.c
+++ average of n num.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int n,i,sum=0,avg;
+int n,i,a[50],sum=0,avg;
 printf("Enter the number:");
 scanf("%d",&n);
-for(i=0;
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+
+  sum=sum+a[i];
+}  
+  printf("sum of n number:%d",sum);
+  avg=sum/n;
+  printf("The averageis:%d",avg);
+  return 0;
+}
