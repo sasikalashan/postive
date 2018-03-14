c073492cf597ce2fc06231d3f22da38bf3020f0c
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-03-14 16:59:34 +0530

    Create print num in words.c

---
+++ print num in words.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number:");
+scanf("%d",&n)
+switch(n)
+{
+case 1:
+printf("One");
+break;
+case 2:
+printf("two");
+break;
+case 3:
+printf("Three");
+break;
+case 4:
+printf("Four");
+break;
+case 5:
+printf("Five");
+break;
+case 6:
+printf("six");
+break;
+case 1:
+printf("One");
+break;
+case 1:
+printf("One");
+break;
+case 1:
+printf("One");
+break;
+case 1:
+printf("One");
+break;
