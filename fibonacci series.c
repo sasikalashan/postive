349ca6a37ab156bd3d8150c1accccb52ba487ed9
sasikalashan <31506059+sasikalashan@users.noreply.github.com>
2018-03-19 15:47:54 +0530

    Update fibonacci series.c

--- fibonacci series.c
+++ fibonacci series.c
@@ -5,7 +5,7 @@ int i,num,f1=0,f2=1,c;
 printf("Enter the range of series:");
 scanf("%d",&num);
 printf("Fibonacci series:");
-for(i=0;i<num;i++)
+for(i=1;i<num;i++)
 {
 printf("\n%d",f1);
 c=f1+f2;
